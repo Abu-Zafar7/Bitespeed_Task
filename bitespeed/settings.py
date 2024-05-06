075325c0384ae90fe5531766e04f74888a354cba
casper-d07 <abuzafars99@gmail.com>
2024-05-06 20:12:04 +0530

    changes made to allowed host

--- bitespeed/settings.py
+++ bitespeed/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-9(#mq0wx_8-#!at^7y33wlxj(gp@738))9-u$vpas+^e9r-pn7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
