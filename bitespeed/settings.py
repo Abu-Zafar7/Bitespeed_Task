a66620f3c0fd3ba880c7b6e9371935a92f7cd6b6
casper-d07 <abuzafars99@gmail.com>
2024-05-06 19:57:24 +0530

    Added requirements

--- bitespeed/settings.py
+++ bitespeed/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
+import dj_database_url
 
 from pathlib import Path
 
@@ -82,7 +83,7 @@ DATABASES = {
     }
 }
 
-
+DATABASES['default'] = dj_database_url.parse("postgres://test_4aqd_user:F69D9UOeMIVLysrEeXwNKpc9THiLg7lO@dpg-corrs420si5c739lgsqg-a.oregon-postgres.render.com/test_4aqd")
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
